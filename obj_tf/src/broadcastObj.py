a79cf85ff76808e27961bcc763256fc37b50370b
zaki <zaki@t490>
2021-02-22 20:46:25 +0800

    Added object list manager and made it more OO

--- obj_tf/src/broadcastObj.py
+++ obj_tf/src/broadcastObj.py
@@ -6,29 +6,24 @@ import tf
 from SimpleNamespace import SimpleNamespace
 from obj_tf.msg import ObjRecognised
 
-objTemplate = SimpleNamespace()
-objTemplate.name = ''
-objTemplate.x = 0 # pos relative to conveyor belt frame
-objTemplate.y = 0 # pos relative to conveyor belt frame
-objTemplate.z = 0
-objTemplate.height = 0
-
 class ObjRecogniser():
-    def __init__(self):
-        self.publisher = rospy.Publisher('/objDetected', ObjRecognised, queue_size=1)
+    def __init__(s):
+        s.publisher = rospy.Publisher('/objDetected', ObjRecognised, queue_size=1)
     
-    def emulateConveyorBelt(s):
-        rospy.Timer(rospy.Duration(5), s.emulateConveyorBeltCallback, oneshot=False)
+    def simulateObjRecognition(s):
+        rospy.Timer(rospy.Duration(5), s.simulateObjRecognitionCallback, oneshot=False)
     
-    def emulateConveyorBeltCallback(s, event):
+    def simulateObjRecognitionCallback(s, event):
         now = rospy.get_rostime()
-        rospy.loginfo("Current time %i %i", now.secs, now.nsecs)
+        #rospy.loginfo("Current time %i %i", now.secs, now.nsecs)
         detectedTime = now
-        print("Obj Found - Send mesg")
-        time2 = rospy.Time.now()
-        rospy.loginfo("Current time %i %i", time2.secs, time2.nsecs)
-        deltaT = time2 - detectedTime
-        print("microseconds taken to print previous line %i s  %i usec", deltaT.secs, deltaT.nsecs/1000)
+        debug = 0
+        if (debug):
+            print("Obj Found - Send mesg")
+            time2 = rospy.Time.now()
+            rospy.loginfo("Current time %i %i", time2.secs, time2.nsecs)
+            deltaT = time2 - detectedTime
+            print("microseconds taken to print previous line %i s  %i usec", deltaT.secs, deltaT.nsecs/1000)
         
         msg = ObjRecognised()
         msg.detectedTime = detectedTime
@@ -39,60 +34,116 @@ class ObjRecogniser():
         s.publisher.publish(msg)
         
 
-    def addObjCallback(event):
-        print 'Timer called at ' + str(event.current_real)
+class ObjOnConveyorBeltListMaintainer:
+    # maintains this list and also assigns a readable TF name
+    # parent_frame: /conveyor_belt
+    # list format:
+    #   name --> msg
+    def __init__(s):
+        s.subscriber = rospy.Subscriber('/objDetected', ObjRecognised, s.subscriberCallback)
+        s.list = []
+        s.number = 0
+        s.listener = tf.TransformListener()
         
-        # what is the distance between conveyor belt and neels cam
-    #     listener = tf.TransformListener()
-    #    try:
-    #        (trans,rot) = listener.lookupTransform('/world', '/neels_cam', rospy.Time(0))
-    #    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-    #        continue
-    #    (trans,rot) = listener.lookupTransform('/world', '/neels_cam', rospy.Time(0))
+    def subscriberCallback(s, msg):
+        print("SUB:------------")        
+        print("SUB: received it")
+        print(msg)
+        print("SUB:------------")
         
+        (trans,rot) = s.listener.lookupTransform('/conveyor_belt', '/neels_cam', rospy.Time(0))        
         
-        newObj.y = trans[1]
+        mapping = { "name": "obj" + str(s.number), \
+                    "detectedTime" : msg.detectedTime, \
+                    "x": trans[0], \
+                    "y": trans[1] \
+                    }        
+        s.number = s.number + 1
+        s.list.append(mapping)
     
+    def getList(s):
+        return s.list
     
+    def removeObjFromList(s, msg):
+        print("removeObjFromList is undefined")
     
-    
-    
+class ObjTfBroadcaster:
+    def __init__(s, objOnConveyorBeltListMaintainer):
+        s.objOnConveyorBeltListMaintainer = objOnConveyorBeltListMaintainer
+        s.broadcaster = tf.TransformBroadcaster()
 
-def broadcastConveyorTf(y):
-    br = tf.TransformBroadcaster()
-    pos = SimpleNamespace()
-    orient = SimpleNamespace()
-    pos.x=0.3
-    #pos.y=1
-    pos.y = y
-    pos.z=0
-    orient.x = 0
-    orient.y = 0
-    orient.z = 0
-    orient.w = 1
-    br.sendTransform((pos.x,pos.y,pos.z),
-                     (orient.x, orient.y, orient.z, orient.w),
-                     rospy.Time.now(),
-                     "/conveyor_belt",
-                     "/world")
+        broadcastFrequency = 50.0
+        rospy.Timer(rospy.Duration(1.0/broadcastFrequency), s.broadcastCallback, oneshot=False)        
+    
+    def broadcastCallback(s, event):
+        list = s.objOnConveyorBeltListMaintainer.getList()
+        for obj in list:
+            #print(obj["name"])
+            name = obj["name"]
+            x = obj["x"]
+            y = obj["y"]
+            s.broadcastObjectTf(name, x, y, z=0)
 
-def broadcastObjectTf(name, x, y, z=0):
-    br = tf.TransformBroadcaster()
-    pos = SimpleNamespace()
-    orient = SimpleNamespace()
-    pos.x= x
-    #pos.y=1
-    pos.y = y
-    pos.z=z
-    orient.x = 0
-    orient.y = 0
-    orient.z = 0
-    orient.w = 1
-    br.sendTransform((pos.x,pos.y,pos.z),
-                     (orient.x, orient.y, orient.z, orient.w),
-                     rospy.Time.now(),
-                     "/" + name,
-                     "/conveyor_belt")
+    def broadcastObjectTf(s, name, x, y, z=0):
+        pos = SimpleNamespace()
+        orient = SimpleNamespace()
+        pos.x= x
+        #pos.y=1
+        pos.y = y
+        pos.z=z
+        orient.x = 0
+        orient.y = 0
+        orient.z = 0
+        orient.w = 1
+        s.broadcaster.sendTransform((pos.x,pos.y,pos.z),
+                         (orient.x, orient.y, orient.z, orient.w),
+                         rospy.Time.now(),
+                         "/" + name,
+                         "/conveyor_belt")
+
+class ConveyorBelt:
+    # broadcast tf
+    def __init__(s):
+        s.broadcaster = tf.TransformBroadcaster()
+        s.currentYPos = 0
+        s.speed = 0
+        s.startTime = rospy.get_time()
+
+        broadcastFrequency = 50.0
+        rospy.Timer(rospy.Duration(1.0/broadcastFrequency), s.broadcastCallback, oneshot=False)                
+
+    def broadcastCallback(s, event):
+        # update position       
+        t = rospy.get_time() - s.startTime
+        print("Broadcasting %f", t)
+        s.currentYPos = s.speed * t
+        
+        # call legacy function, can't be bothered to refactor this
+        s.broadcastConveyorTf(s.currentYPos)
+
+    def simulateMovement(s):
+        s.startTime = rospy.get_time()
+        s.speed = 0.30 # m/s
+        
+        
+    def broadcastConveyorTf(s, y):
+        pos = SimpleNamespace()
+        orient = SimpleNamespace()
+        pos.x=0.3
+        #pos.y=1
+        pos.y = y
+        pos.z=0
+        orient.x = 0
+        orient.y = 0
+        orient.z = 0
+        orient.w = 1
+        s.broadcaster.sendTransform((pos.x,pos.y,pos.z),
+                         (orient.x, orient.y, orient.z, orient.w),
+                         rospy.Time.now(),
+                         "/conveyor_belt",
+                         "/world")
+
+
 
 def roboArmFollow(name):
     pass
@@ -100,14 +151,23 @@ def roboArmFollow(name):
 def run():
 
     objRecogniser = ObjRecogniser()
-    objRecogniser.emulateConveyorBelt()    
+    objRecogniser.simulateObjRecognition()    
+
+    # wait a bit to initialise subscriber
+    rospy.sleep(1)
+    
+    conveyorBelt = ConveyorBelt()
+    conveyorBelt.simulateMovement()    
+    
+    
+    objOnConveyorBeltListMaintainer = ObjOnConveyorBeltListMaintainer()   
+    
+    objTfBroadcaster = ObjTfBroadcaster(objOnConveyorBeltListMaintainer)
     
     rospy.loginfo("Let's go")
 
     rate = rospy.Rate(1)
-
-    conveyorSpeed = 0.01 # m / sec    
-    
+  
     startTime = rospy.get_time()
     
     # start program to introduce items onto the belt
@@ -115,17 +175,17 @@ def run():
     
     while not rospy.is_shutdown():
 
-        simTime = rospy.get_time() - startTime
-        t = simTime
-        print("Broadcasting %f", t)
-        positiony = conveyorSpeed * t
-        broadcastConveyorTf(positiony)
+#        simTime = rospy.get_time() - startTime
+#        t = simTime
+#        print("Broadcasting %f", t)
+#        positiony = conveyorSpeed * t
+        #broadcastConveyorTf(positiony)
         # calc form Camera's frame where is the object's frame and input it 
         # below
         # tf echo new_obj /camera
         # store this data somewhere
-        broadcastObjectTf('obj1', 0.05, -0.5)
-        broadcastObjectTf('obj2', -0.05, -0.75)
+        #broadcastObjectTf('obj1', 0.05, -0.5)
+        #broadcastObjectTf('obj2', -0.05, -0.75)
         
         # also add it to moveit's planning scene
         #box_pose = geometry_msgs.msg.PoseStamped()
