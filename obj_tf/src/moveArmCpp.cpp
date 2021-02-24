9fd748b10a6795e17121b142594297b833d9d254
zaki <zaki@t490>
2021-02-24 22:12:50 +0800

    Descartes now runs on Niryo

--- obj_tf/src/moveArmCpp.cpp
+++ obj_tf/src/moveArmCpp.cpp
@@ -1,166 +1,260 @@
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2013, SRI International
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of SRI International nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
-
-/* Author: Sachin Chitta, Dave Coleman, Mike Lautman */
-
-// Zaki modified it
-
+// Core ros functionality like ros::init and spin
 #include <ros/ros.h>
+// ROS Trajectory Action server definition
+#include <control_msgs/FollowJointTrajectoryAction.h>
+// Means by which we communicate with above action-server
+#include <actionlib/client/simple_action_client.h>
 
-#include <moveit/move_group_interface/move_group_interface.h>
-#include <moveit/planning_scene_interface/planning_scene_interface.h>
+// Includes the descartes robot model we will be using
+#include <descartes_moveit/ikfast_moveit_state_adapter.h>
 
-#include <moveit_msgs/DisplayRobotState.h>
-#include <moveit_msgs/DisplayTrajectory.h>
+// Includes the descartes trajectory type we will be using
+#include <descartes_trajectory/axial_symmetric_pt.h>
+#include <descartes_trajectory/cart_trajectory_pt.h>
 
-#include <moveit_msgs/AttachedCollisionObject.h>
-#include <moveit_msgs/CollisionObject.h>
+// Includes the planner we will be using
+#include <descartes_planner/dense_planner.h>
 
-#include <moveit_visual_tools/moveit_visual_tools.h>
+// Includes the utility function for converting to trajectory_msgs::JointTrajectory's
+#include <descartes_utilities/ros_conversions.h>
+
+/**
+ * Makes a dummy trajectory for the robot to follow.
+ */
+std::vector<descartes_core::TrajectoryPtPtr> makePath();
+
+/**
+ * Sends a ROS trajectory to the robot controller
+ */
+bool executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory);
 
 int main(int argc, char** argv)
 {
+  // Initialize ROS
   ros::init(argc, argv, "move_arm_node");
-  ros::NodeHandle node_handle;
-  ros::AsyncSpinner spinner(1);
+  ros::NodeHandle nh;
+
+  // Since we're not calling ros::spin() and doing the planning in a callback, but rather just handling this
+  // inline, we need to create an async spinner if our publishers are to work. Note that many MoveIt components
+  // will also not work without an active spinner and Descartes uses moveit for its "groups" and "scene" descriptions
+  ros::AsyncSpinner spinner (1);
   spinner.start();
 
-  // BEGIN_TUTORIAL
-  //
-  // Setup
-  // ^^^^^
-  //
-  // MoveIt! operates on sets of joints called "planning groups" and stores them in an object called
-  // the `JointModelGroup`. Throughout MoveIt! the terms "planning group" and "joint model group"
-  // are used interchangably.
-  static const std::string PLANNING_GROUP = "arm";
+  // 1. First thing first, let's create a kinematic model of the robot. In Descartes, this is used to do things
+  // like forward kinematics (joints -> pose), inverse kinematics (pose -> many joints), and collision checking.
 
-  // The :move_group_interface:`MoveGroup` class can be easily
-  // setup using just the name of the planning group you would like to control and plan for.
-  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+  // All of the existing planners (as of Nov 2017) have been designed with the idea that you have "closed form"
+  // kinematics. This means that the default solvers in MoveIt (KDL) will NOT WORK WELL. I encourage you to produce
+  // an ikfast model for your robot (see MoveIt tutorial) or use the OPW kinematics package if you have a spherical
+  // wrist industrial robot. See http://docs.ros.org/kinetic/api/moveit_tutorials/html/doc/ikfast/ikfast_tutorial.html 
 
-  // We will use the :planning_scene_interface:`PlanningSceneInterface`
-  // class to add and remove collision objects in our "virtual world" scene
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  // This package assumes that the move group you are using is pointing to an IKFast kinematics plugin in its
+  // kinematics.yaml file. By default, it assumes that the underlying kinematics are from 'base_link' to 'tool0'.
+  // If you have renamed these, please set the 'ikfast_base_frame' and 'ikfast_tool_frame' parameter (not in the
+  // private namespace) to the base and tool frame used to generate the IKFast model.
 
-  // Raw pointers are frequently used to refer to the planning group for improved performance.
-  const robot_state::JointModelGroup* joint_model_group =
-      move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+  descartes_core::RobotModelPtr model (new descartes_moveit::IkFastMoveitStateAdapter());
 
-  // Visualization
-  // ^^^^^^^^^^^^^
-  //
-  // The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
-  // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script
-  namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools("tool_link");
-  visual_tools.deleteAllMarkers();
+  // Name of description on parameter server. Typically just "robot_description". Used to initialize
+  // moveit model.
+  const std::string robot_description = "robot_description";
 
-  // Remote control is an introspection tool that allows users to step through a high level script
-  // via buttons and keyboard shortcuts in RViz
-  visual_tools.loadRemoteControl();
+  // name of the kinematic group you defined when running MoveitSetupAssistant. For many industrial robots this will be
+  // "manipulator"
+  const std::string group_name = "arm";
 
-  // RViz provides many types of markers, in this demo we will use text, cylinders, and spheres
-  Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
-  text_pose.translation().x() = 0.1;
-  text_pose.translation().y() = 0.1;
-  text_pose.translation().z() = 0.1;
-  visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
+  // Name of frame in which you are expressing poses. Typically "world_frame" or "base_link".
+  const std::string world_frame = "base_link";
 
-  // Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations
-  visual_tools.trigger();
+  // tool center point frame (name of link associated with tool). The robot's flange is typically "tool0" but yours
+  // could be anything. We typically have our tool's positive Z-axis point outward from the grinder, welder, etc.
+  const std::string tcp_frame = "tool_link";
 
-  // Getting Basic Information
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
-  // We can print the name of the reference frame for this robot.
-  ROS_INFO_NAMED("tutorial", "Reference frame: %s", move_group.getPlanningFrame().c_str());
+  if (nh.hasParam("ikfast_tool_frame"))
+  {
+    ROS_INFO("Found 'ikfast_tool_frame'");
+  }
+  else
+  {
+    ROS_INFO("No param named 'ikfast_tool_frame'");
+  }
 
-  // We can also print the name of the end-effector link for this group.
-  ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group.getEndEffectorLink().c_str());
 
-  // Start the demo
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+  // Before you can use a model, you must call initialize. This will load robot models and sanity check the model.
+  if (!model->initialize(robot_description, group_name, world_frame, tcp_frame))
+  {
+    ROS_INFO("Could not initialize robot model");
+    return -1;
+  }
 
-  // Planning to a Pose goal
-  // ^^^^^^^^^^^^^^^^^^^^^^^
-  // We can plan a motion for this group to a desired pose for the
-  // end-effector.
-  geometry_msgs::Pose target_pose1;
-  target_pose1.orientation.w = 1.0;
-  target_pose1.position.x = 0.2;
-  target_pose1.position.y = 0.0;
-  target_pose1.position.z = 0.15;
-  move_group.setPoseTarget(target_pose1);
+  model->setCheckCollisions(true); // Let's turn on collision checking.
 
-  // Now, we call the planner to compute the plan and visualize it.
-  // Note that we are just planning, not asking move_group
-  // to actually move the robot.
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+  // 2. The next thing to do is to generate a path for the robot to follow. The description of this path is one of the
+  // cool things about Descartes. The source of this path is where this library ties into your application: it could
+  // come from CAD or from surfaces that were "scanned".
 
-  bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  // Make the path by calling a helper function. See makePath()'s definition for more discussion about paths.
+  std::vector<descartes_core::TrajectoryPtPtr> points = makePath();
 
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+  // 3. Now we create a planner that can fuse your kinematic world with the points you want to move the robot
+  // along. There are a couple of planners now. DensePlanner is the naive, brute force approach to solving the
+  // trajectory. SparsePlanner may be faster for some problems (especially very dense ones), but has recieved
+  // less overall testing and evaluation.
+  descartes_planner::DensePlanner planner;
 
-  // Visualizing plans
-  // ^^^^^^^^^^^^^^^^^
-  // We can also visualize the plan as a line with markers in RViz.
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 as trajectory line");
-  visual_tools.publishAxisLabeled(target_pose1, "pose1");
-  visual_tools.publishText(text_pose, "Pose Goal", rvt::WHITE, rvt::XLARGE);
-  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
-  visual_tools.trigger();
-  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+  // Like the model, you also need to call initialize on the planner
+  if (!planner.initialize(model))
+  {
+    ROS_ERROR("Failed to initialize planner");
+    return -2;
+  }
 
-  // Moving to a pose goal
-  // ^^^^^^^^^^^^^^^^^^^^^
-  //
-  // Moving to a pose goal is similar to the step above
-  // except we now use the move() function. Note that
-  // the pose goal we had set earlier is still active
-  // and so the robot will try to move to that goal. We will
-  // not use that function in this tutorial since it is
-  // a blocking function and requires a controller to be active
-  // and report success on execution of a trajectory.
+  // 4. Now, for the planning itself. This typically happens in two steps. First, call planPath(). This function takes
+  // your input trajectory and expands it into a large kinematic "graph". Failures at this point indicate that the
+  // input path may not have solutions at a given point (because of reach/collision) or has two points with no way
+  // to connect them.
+  if (!planner.planPath(points))
+  {
+    ROS_ERROR("Could not solve for a valid path");
+    return -3;
+  }
 
-  /* Uncomment below line when working with a real robot */
-  /* move_group.move(); */
-  move_group.move();
+  // After expanding the graph, we now call 'getPath()' which searches the graph for a minimum cost path and returns
+  // the result. Failures here (assuming planPath was good) indicate that your path has solutions at every waypoint
+  // but constraints prevent a solution through the whole path. Usually this means a singularity is hanging out in the
+  // middle of your path: the robot can solve all the points but not in the same arm configuration.
+  std::vector<descartes_core::TrajectoryPtPtr> result;
+  if (!planner.getPath(result))
+  {
+    ROS_ERROR("Could not retrieve path");
+    return -4;
+  }
 
-  // END_TUTORIAL
+  // 5. Translate the result into something that you can execute. In ROS land, this means that we turn the result into
+  // a trajectory_msgs::JointTrajectory that's executed through a control_msgs::FollowJointTrajectoryAction. If you
+  // have your own execution interface, you can get joint values out of the results in the same way.
 
-  ros::shutdown();
+  // get joint names - this could be from the robot model, or from the parameter server.
+  std::vector<std::string> names;
+  nh.getParam("controller_joint_names", names);
+
+  ROS_INFO("length of joints: %lu", names.size() );
+  for ( auto name : names)
+  {
+	ROS_INFO("name of joints: %s", name.c_str() );
+  }
+  
+
+  // Create a JointTrajectory
+  trajectory_msgs::JointTrajectory joint_solution;
+  joint_solution.joint_names = names;
+
+  // Define a default velocity. Descartes points without specified timing will use this value to limit the
+  // fastest moving joint. This usually effects the first point in your path the most.
+  const static double default_joint_vel = 0.5; // rad/s
+  if (!descartes_utilities::toRosJointPoints(*model, result, default_joint_vel, joint_solution.points))
+  {
+    ROS_ERROR("Unable to convert Descartes trajectory to joint points");
+    return -5;
+  }
+
+  ROS_INFO("Reached this point");
+
+
+  // 6. Send the ROS trajectory to the robot for execution
+  if (!executeTrajectory(joint_solution))
+  {
+    ROS_ERROR("Could not execute trajectory!");
+    return -6;
+  }
+
+  // Wait till user kills the process (Control-C)
+  ROS_INFO("Done!");
   return 0;
+}
+
+descartes_core::TrajectoryPtPtr makeCartesianPoint(const Eigen::Isometry3d& pose, double dt)
+{
+  using namespace descartes_core;
+  using namespace descartes_trajectory;
+
+  return TrajectoryPtPtr( new CartTrajectoryPt( TolerancedFrame(pose), TimingConstraint(dt)) );
+}
+
+descartes_core::TrajectoryPtPtr makeTolerancedCartesianPoint(const Eigen::Isometry3d& pose, double dt)
+{
+  using namespace descartes_core;
+  using namespace descartes_trajectory;
+  return TrajectoryPtPtr( new AxialSymmetricPt(pose, M_PI / 12.0, AxialSymmetricPt::Z_AXIS, TimingConstraint(dt)) );
+}
+
+std::vector<descartes_core::TrajectoryPtPtr> makePath()
+{
+  // In Descartes, trajectories are composed of "points". Each point describes what joint positions of the robot can
+  // satisfy it. You can have a "joint point" for which only a single solution is acceptable. You might have a
+  // fully defined cartesian point for which many (8 or 16) different robot configurations might work. You could
+  // allow extra tolerances in any of these and even more points satisfy the constraints.
+
+  // In this first tutorial, we're just going to describe a simple cartesian trajectory that moves the robot
+  // along a line in the XY plane.
+
+  // Step 1: Let's start by just doing the math to generate the poses we want.
+
+  // First thing, let's generate a pattern with its origin at zero. We'll define another transform later that
+  // can move it to somewere more convenient.
+  const static double step_size = 0.001;
+  const static int num_steps = 20;
+  const static double time_between_points = 0.5;
+
+  EigenSTL::vector_Isometry3d pattern_poses;
+  for (int i = -num_steps / 2; i < num_steps / 2; ++i)
+  {
+    // Create a pose and initialize it to identity
+    Eigen::Isometry3d pose = Eigen::Isometry3d::Identity();
+    // set the translation (we're moving along a line in Y)
+    pose.translation() = Eigen::Vector3d(0, i * step_size, 0);
+    // set the orientation. By default, the tool will be pointing up into the air when we usually want it to
+    // be pointing down into the ground.
+    pose *= Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX()); // this flips the tool around so that Z is down
+    pattern_poses.push_back(pose);
+  }
+
+  // Now lets translate these points to Descartes trajectory points
+  // The ABB2400 is pretty big, so let's move the path forward and up.
+  Eigen::Isometry3d pattern_origin = Eigen::Isometry3d::Identity();
+  pattern_origin.translation() = Eigen::Vector3d(0.1, 0.1, 0.2);
+
+  std::vector<descartes_core::TrajectoryPtPtr> result;
+  for (const auto& pose : pattern_poses)
+  {
+    // This creates a trajectory that searches around the tool Z and let's the robot move in that null space
+    descartes_core::TrajectoryPtPtr pt = makeTolerancedCartesianPoint(pattern_origin * pose, time_between_points);
+    // This creates a trajectory that is rigid. The tool cannot float and must be at exactly this point.
+    //  descartes_core::TrajectoryPtPtr pt = makeCartesianPoint(pattern_origin * pose, time_between_points);
+    result.push_back(pt);
+  }
+
+  // Note that we could also add a joint point representing the starting location of the robot, or a joint point
+  // representing the desired end pose of the robot to the front and back of the vector respectively.
+
+  return result;
+}
+
+bool executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory)
+{
+  // Create a Follow Joint Trajectory Action Client
+  actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac (
+	"niryo_one_follow_joint_trajectory_controller/follow_joint_trajectory", true);
+
+  if (!ac.waitForServer(ros::Duration(2.0)))
+  {
+    ROS_ERROR("Could not connect to action server");
+    return false;
+  }
+
+  control_msgs::FollowJointTrajectoryGoal goal;
+  goal.trajectory = trajectory;
+  goal.goal_time_tolerance = ros::Duration(1.0);
+  
+  return ac.sendGoalAndWait(goal) == actionlib::SimpleClientGoalState::SUCCEEDED;
 }
