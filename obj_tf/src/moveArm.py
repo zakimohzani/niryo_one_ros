449d69ccca54428c1a34d0369a416eb1e53319ba
zaki <zaki@t490>
2021-04-06 16:14:35 +0800

    Backup. State machine skeleton is placed into code.

--- obj_tf/src/moveArm.py
+++ obj_tf/src/moveArm.py
@@ -16,9 +16,62 @@ from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 
 
+global moveitNs
+moveitNs = SimpleNamespace()
 
-def roboArmFollow(name):
-    pass
+global currentState
+
+def state_look_for_new_obj():
+    global currentState # required for the state machine
+    rospy.loginfo('state = state_look_for_new_obj')
+
+    # do something
+    group = moveitNs.group
+    
+    start_pose = geometry_msgs.msg.Pose()
+    start_pose.orientation.w = 1.0
+    start_pose.position.x = 0.3
+    start_pose.position.y = -0.3
+    start_pose.position.z = 0.2
+
+    rospy.loginfo("Let's go")
+
+
+    group.set_pose_target(start_pose)
+    
+    plan = group.go(wait=True)
+    # Calling `stop()` ensures that there is no residual movement
+    group.stop()
+    # It is always good to clear your targets after planning with poses.
+    # Note: there is no equivalent function for clear_joint_value_targets()
+    group.clear_pose_targets()
+
+
+    # listen for objects now
+    listener = tf.TransformListener()
+
+    # if event has been created then change state
+    currentState = 'state_picking_up_obj';
+
+def state_picking_up_obj():
+    global currentState # required for the state machine
+    rospy.loginfo('state = state_picking_up_obj')
+
+    # do something
+    
+    # if event has been created then change state
+    currentState = 'state_picked_up_obj_and_dumping_it';
+
+def state_picked_up_obj_and_dumping_it():
+    global currentState # required for the state machine
+    rospy.loginfo('state = state_picked_up_obj_and_dumping_it')
+
+    # do something
+    
+    # if event has been created then change state
+    currentState = 'state_look_for_new_obj';
+
+
 
 def run():
     moveit_commander.roscpp_initialize(sys.argv)
@@ -48,94 +101,26 @@ def run():
     print robot.get_current_state()
     print ""  
     
-    
-    start_pose = geometry_msgs.msg.Pose()
-    start_pose.orientation.w = 1.0
-    start_pose.position.x = 0.3
-    start_pose.position.y = -0.3
-    start_pose.position.z = 0.2
+    # store some of these variables so that states can access it
+    global moveitNs    
+    moveitNs.group = group
 
-    rospy.loginfo("Let's go")
+    global currentState
+    currentState = 'state_look_for_new_obj'
 
-
-    group.set_pose_target(start_pose)
-    
-    plan = group.go(wait=True)
-    # Calling `stop()` ensures that there is no residual movement
-    group.stop()
-    # It is always good to clear your targets after planning with poses.
-    # Note: there is no equivalent function for clear_joint_value_targets()
-    group.clear_pose_targets()
-
-
-    # listen for objects now
-    listener = tf.TransformListener()
-
-
-
-    rate = rospy.Rate(10)
-    startTime = rospy.get_time()    
-    
-    group.set_planning_time(0.1) # seconds
-    
     while not rospy.is_shutdown():
+        # Implicit state machine. Based on code from: 
+        # https://stackoverflow.com/questions/7936572/python-call-a-function-from-string-name
+        possibles = globals().copy()
+        possibles.update(locals())
+        state = possibles.get(currentState)
+        if not state:
+             raise NotImplementedError("State %s not implemented" % state_name)
+        state()
+        # End of state machine code    
+        
+        time.sleep(5)
 
-        try:
-            (trans,rot) = listener.lookupTransform('/world', '/obj0', rospy.Time(0))
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-#            group.set_pose_target(start_pose)
-#            
-#            plan = group.go(wait=True)
-#            # Calling `stop()` ensures that there is no residual movement
-#            group.stop()
-#            # It is always good to clear your targets after planning with poses.
-#            # Note: there is no equivalent function for clear_joint_value_targets()
-#            group.clear_pose_targets()
-            continue
-
-        # robot's frame is also world so we don't need any transform
-        
-        
-        target_pose = geometry_msgs.msg.Pose()
-        target_pose.orientation.w = 1.0
-        target_pose.position.x = trans[0]
-        target_pose.position.y = trans[1]
-        target_pose.position.z = 0.2
-    
-        group.set_pose_target(target_pose)
-        
-        
-        # BUG: Niryo driver's doesn't actually wait for the movement to complete
-        # before sending a 'complete' signal to Moveit
-        # Hence, the following sentence is not a blocking statement
-        # setting the wait-timer too low would mean the robot will never move
-        # because plan after plan after plan are continously being streamed to 
-        # it
-        plan = group.go(wait=True)
-
-        """
-        plan = group.plan()
-        
-        print("Plan output:")
-        #print(plan)        
-        print(len(plan.joint_trajectory.points))
-        
-        group.execute(plan, wait=True)
-        """
-        
-
-        #print("Stopping arm")
-        # Calling `stop()` ensures that there is no residual movement
-        #group.stop()
-        # It is always good to clear your targets after planning with poses.
-        # Note: there is no equivalent function for clear_joint_value_targets()
-        #group.clear_pose_targets()
-
-        simTime = rospy.get_time() - startTime
-        t = simTime
-        print("Sim time: %f", t)
-        
-        rate.sleep()
 
 if __name__ == '__main__':
     try:
